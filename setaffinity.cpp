4006b5e625490fd9d69320174f9df5d019c70316
Chuntao Hong <chuntao.hong@gmail.com>
2012-01-30 13:36:04 +0800

    modified the maximum size of mask to sizeof(DWORD_PTR)*8

--- setaffinity.cpp
+++ setaffinity.cpp
@@ -7,51 +7,52 @@ using namespace std;
 
 int main(int argc, char ** argv)
 {
-	if(argc!=3)
-	{
-		cout<<"usage: "<< argv[0] << " pid mask" <<endl;
-		return 1;
-	}
+    if(argc!=3)
+    {
+        cout<<"usage: "<< argv[0] << " pid mask" <<endl;
+        return 1;
+    }
 
-	int pid = 0;
-	istringstream sspid(argv[1]);
-	sspid>>pid;
-	if(pid<=0)
-	{
-		cout<<"bad pid"<<endl;
-		return 1;
-	}
+    int pid = 0;
+    istringstream sspid(argv[1]);
+    sspid>>pid;
+    if(pid<=0)
+    {
+        cout<<"bad pid"<<endl;
+        return 1;
+    }
 
-	HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, TRUE, pid);
-	if(!hProcess)
-	{
-		cout<<"cannot open the process"<<endl;
-		return 1;
-	}
+    HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, TRUE, pid);
+    if(!hProcess)
+    {
+        cout<<"cannot open the process"<<endl;
+        return 1;
+    }
 
-	string mask_str = argv[2];
-	if(mask_str.size()>32)
-	{
-		cout<<"mask is too long"<<endl;
-		return 1;
-	}
+    string mask_str = argv[2];
 	DWORD_PTR mask = 0;
-	for(int i=mask_str.size()-1; i>=0;i--)
-	{
-		mask = mask<<1;
-		if(mask_str[i]=='1')
-		{
-			mask |= 1;
-		}
-	}
+    if(mask_str.size()>sizeof(mask)*8)
+    {
+        cout<<"mask is too long: mask_size="<<mask_str.size()<<", maximum size="<<sizeof(mask)*8<<endl;
+        return 1;
+    }
 
-	if( !SetProcessAffinityMask(hProcess, mask) )
-	{
-		cout<<"error setting affinity mask: "<<mask<<endl;
-		cout<<"error num: "<<GetLastError()<<endl;
-		CloseHandle(hProcess);
-		return 1;
-	}
-	CloseHandle(hProcess);
-	return 0;
+    for(int i=mask_str.size()-1; i>=0;i--)
+    {
+        mask = mask<<1;
+        if(mask_str[i]=='1')
+        {
+            mask |= 1;
+        }
+    }
+
+    if( !SetProcessAffinityMask(hProcess, mask) )
+    {
+        cout<<"error setting affinity mask: "<<mask<<endl;
+        cout<<"error num: "<<GetLastError()<<endl;
+        CloseHandle(hProcess);
+        return 1;
+    }
+    CloseHandle(hProcess);
+    return 0;
 }
